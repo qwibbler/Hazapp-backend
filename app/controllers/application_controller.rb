5347846ed53dd575b2f4da49d7fc64aaf5759428
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-12-19 14:52:21 -0600

    Add premaslas to maslas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def pivot_table
+    @table = PivotTable::Grid.new do |g|
+      g.source_data  = PreMasla.all
+      g.column_name  = :premasla
+      g.row_name     = :masla
+      g.field_name   = :value
+    end
+    @table.build
+  end
 end
