8ea73663d5895218092f73b9ba491e9f59c10362
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-12-21 06:34:59 -0600

    Update table

--- app/helpers/maslas_helper.rb
+++ app/helpers/maslas_helper.rb
@@ -1,2 +1,9 @@
 module MaslasHelper
+  def style_date(date)
+    date.strftime("%d/%m/%Y at %I:%M %p")
+  end
+
+  def convert_date(date)
+    style_date(Time.at(date.to_f / 1000)) if date.to_i.to_s == date
+  end
 end
