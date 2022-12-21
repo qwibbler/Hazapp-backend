bed60f853b8b7bc48b0fcad420812dede7ede346
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-12-21 07:45:23 -0600

    Style entries

--- app/helpers/maslas_helper.rb
+++ app/helpers/maslas_helper.rb
@@ -6,4 +6,8 @@ module MaslasHelper
   def convert_date(date)
     style_date(Time.at(date.to_f / 1000)) if date.to_i.to_s == date
   end
+
+  def style_entries(entries)
+    entries.map { |entry| entry.gsub(/["{}\\]/, '').gsub('Time', '') }
+  end
 end
