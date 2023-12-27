d9b5eba6a1475a9e7e4888c80a6b8a7c0834fab1
qwibbler <hmemaz@gmail.com>
2023-12-27 01:52:45 -0600

    Add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         def generate_jwt
-          JWT.encode({ id: id,
-                      exp: 60.days.from_now.to_i },
-                     Rails.application.secrets.secret_key_base)
-        end
+        #  def generate_jwt
+        #   JWT.encode({ id: id,
+        #               exp: 60.days.from_now.to_i },
+        #              Rails.application.secrets.secret_key_base)
+        # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   resources :maslas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  scope :api, defaults: { format: :json } do
-    devise_for :users
-  end
+  # scope :api, defaults: { format: :json } do
+  #   devise_for :users
+  # end
 
   # Defines the root path route ("/")
   root "maslas#index"
