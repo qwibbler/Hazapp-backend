564d95952f88217257a079353fb314c152aec266
qwibbler <hmemaz@gmail.com>
2023-12-31 11:27:32 -0600

    Allow cors only from hazapp

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource(
-     '*',
+     'http://localhost:8080',
      headers: :any,
      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
      methods: [:get, :patch, :put, :delete, :post, :options, :show]
