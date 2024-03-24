904c23f716629f83f781a1241fffe542090df657
qwibbler <hmemaz@gmail.com>
2024-03-24 10:48:39 -0500

    Final updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-      sessions: 'users/sessions',
-  }
+  devise_for :users, controllers: {sessions: 'users/sessions'}
   resources :maslas
   resources :users, only: [:index, :show, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
