35895a4cf7e2d23ea5f4ca1add1530e9ba272714
qwibbler <hmemaz@gmail.com>
2024-03-24 19:21:34 -0500

    Fix noMaslas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {sessions: 'users/sessions'}
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :maslas
-  resources :users, only: [:index, :show, :update]
+  resources :users, only: %i[index show update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "maslas#index"
+  root 'maslas#index'
 end
